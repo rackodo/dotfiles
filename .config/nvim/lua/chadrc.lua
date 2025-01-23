52a4db2a38e2bc556afa401ecc181aa7e8c508e0
Bash Elliott <bashelliott@gmail.com>
2025-01-24 02:21:24 +1100

    nvchad theme set

--- .config/nvim/lua/chadrc.lua
+++ .config/nvim/lua/chadrc.lua
@@ -6,7 +6,7 @@
 local M = {}
 
 M.base46 = {
-	theme = "onedark",
+	theme = "monekai",
 
 	-- hl_override = {
 	-- 	Comment = { italic = true },
@@ -21,4 +21,9 @@ M.base46 = {
 --      }
 --}
 
+M.nvdash = {
+	load_on_startup = true,
+}
+
 return M
+
