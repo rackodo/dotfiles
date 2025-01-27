851c34bfa93cd74d23bb687765f5e6c1f42a31ba
Bash Elliott <bashelliott@gmail.com>
2025-01-27 11:35:09 +1100

    sure

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,8 +10,12 @@ alias royalflush='brew update && brew upgrade && brew cleanup --prune=all && bre
 alias vim='nvim'
 alias clock='tty-clock -c -s -t -C 6' # needs tty-clock (brew install tty-clock)
 alias myip='nslookup myip.opendns.com resolver1.opendns.com | grep 'Address' | sed -n 2p | cut -c10-'
+alias z='zoxide'
 
 # Default
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+zoxide init fish | source
+fzf --fish | source
