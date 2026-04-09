920064d4d9bcdc42ebf02eb3683abe8f20a3c88d
Bash Elliott <bashelliott@gmail.com>
2026-04-09 17:27:03 +1000

    add neovim config w/ packer

---
+++ .config/nvim/init.lua
@@ -0,0 +1,11 @@
+-- use packer
+require('plugins')
+
+-- make background transparent
+vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'Pmenu', { bg = 'none' })
+
+-- set gruvbox theme
+vim.cmd([[colorscheme gruvbox]])
